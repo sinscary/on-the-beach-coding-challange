2c32c88aef70a33725786e224fecd21c1b58bd57
sinscary <nbsharma@outlook.com>
2019-11-04 10:23:10 +0530

    add logic to process jobs

--- processor.rb
+++ processor.rb
@@ -4,6 +4,33 @@ class Processor
   
   def initialize(jobs: {})
     @jobs = jobs
+    @ordered_jobs = []
+    @job_queue = []
+  end
+
+  def process
+    @jobs.each do |job, dependency_job|
+      raise Exceptions::SelfDependencyError.new(
+        "Job can not depend on itself."
+      ) if job == dependency_job
+      raise Exceptions::CircularDependencyError.new(
+        "Circular dependency found"
+      ) if @job_queue.include?(dependency_job)
+      order_jobs(job, dependency_job)
+    end
+    (@ordered_jobs |= @job_queue).uniq
+    @ordered_jobs.join()
+  end
+
+  private
+  def order_jobs(job, dependency_job=nil)
+    @job_queue.push(job) if @job_queue.empty?
+    unless dependency_job.nil?
+      @job_queue.unshift(dependency_job)
+      return
+    end
+    @ordered_jobs |= @job_queue
+    @job_queue = []
   end
 
 end
